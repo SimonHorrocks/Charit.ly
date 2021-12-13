16b55b5b945c5023af3e0644ddb569ad03c37bfc
Joe <J.Burley1@newcastle.ac.uk>
2021-12-13 04:29:55 +0000

    def __init__ for each class in Models

--- models.py
+++ models.py
@@ -2,42 +2,69 @@
 from app import db
 
 
+
+def init_db():
+
+
+
+
+
 class User(db.Model):
     __tablename__ = 'User'
-
-    UserID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Username = db.Column(db.String(30), nullable=False, unique=True)
     Email = db.Column(db.String(100), nullable=False, unique=True)
     Password = db.Column(db.String(100), nullable=False)
     RoleID = db.Column(db.String(100), nullable=False)
     EncryptionKey = db.Column(db.String(100), nullable=False)
 
+    def __init__(self, username, email, password, roleid, encryptionkey):
+        self.Username=username
+        self.Email=email
+        self.Password=password
+        self.RoleID=roleid
+        self.EncryptionKey=encryptionkey
+
 
 class Page(db.Model):
     __tablename__ = 'Page'
 
-    PageID = db.Column(db.Integer, primary_key=True)
+    PageID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     PageName = db.Column(db.VARCHAR(100), nullable=False)
     PageDescription = db.Column(db.TEXT)
     PageOwner = db.Column(db.Integer, db.ForeignKey('User.UserID'))
     TimeCreated = db.Column(db.DateTime)
     Followers = db.Column(db.Integer, nullable=False, default=0)
 
+    def __init__(self, pagename, pagedescription, pageowner, timecreated, followers):
+        self.PageName=pagename
+        self.PageDescription=pagedescription
+        self.PageOwner=pageowner
+        self.TimeCreated=timecreated
+        self.Followers=followers
+
 
 class Post(db.Model):
     __tablename__ = 'Post'
 
-    PostID = db.column(db.Integer, primary_key=True)
+    PostID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     PostTitle = db.Column(db.VARCHAR(100), nullable=False)
     PostContent = db.Column(db.VARCHAR(100))
     Page = db.column(db.Integer, db.ForeignKey('Page.PageID'))
     TimeCreated = db.Column(db.DateTime)
 
+    def __init__(self, posttitle, postcontent, page, timecreated):
+        self.PostTitle=posttitle
+        self.PostContent=postcontent
+        self.Page=page
+        self.TimeCreated=timecreated
+
+
 
 class Event(db.Model):
     __tablename__ = 'Event'
 
-    EventID = db.Column(db.Integer, primary_key=True)
+    EventID = db.Column(db.Integer, primary_key=True, autoIncrement=True)
     EventName = db.Column(db.VARCHAR(100))
     EventDescription = db.Column(db.VARCHAR(100))
     EventTime = db.Column(db.VARCHAR(100))
@@ -45,12 +72,24 @@ class Event(db.Model):
     Page = db.column(db.Integer, db.ForeignKey('Page.PageID'))
     EventLocation = db.Column(db.VARCHAR(100))
 
+    def __init__(self, eventname, eventdescription, eventtime, timecreated, page, eventlocation):
+        self.EventName=eventname
+        self.EventDescription=eventdescription
+        self.EventTime=eventtime
+        self.TimeCreated=timecreated
+        self.Page=page
+        self.EventLocation=eventlocation
+
+
 
 class Interest(db.Model):
     __tablename__ = 'Interest'
 
     InterestID = db.column(db.Integer, primary_key=True)
     Interest = db.column(db.VARCHAR(100))
+
+    def __init__(self, interest):
+        self.Interest=interest
 
 
 # Many to Many Relationships
