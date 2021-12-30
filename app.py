ccc4e8ea78b834c45756131d0e0bc740ad63a98d
Kalina <k.hajzner1@ncl.ac.uk>
2021-12-30 10:48:42 +0000

    Fixed small issue with the database's configuration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://csc2033_team13:LashFain;own@localhost:42069:3306/csc1033_team13'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://csc2033_team13:LashFain;own@localhost:420693306/csc1033_team13'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
