a2871b44ab14dd4cac4495c0ba668399b8ce7e84
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-05 14:32:03 +0000

    Updated registration form.

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,8 @@ from wtforms import StringField, SubmitField, PasswordField
 
 
 class RegisterForm(FlaskForm):
+    email = StringField()
     username = StringField()
     password = PasswordField()
+    confirm_password = PasswordField()
     submit = SubmitField()
