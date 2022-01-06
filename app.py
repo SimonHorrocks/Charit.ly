680cf3494747f8eb94d11a0b407dd4dda0a3064e
Joe <J.Burley1@newcastle.ac.uk>
2022-01-06 03:30:04 +0000

    create local database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///charityForum.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -10,3 +15,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
---
+++ models.py
@@ -0,0 +1,55 @@
+from app import db
+import datetime
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    roleID = db.Column(db.String(100), nullable=False, default='user')
+    pages = db.relationship('Page', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username})', '{self.email}' , '{self.role}')"
+
+
+class Page(db.Model):
+    __tablename__ = 'page'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    time_created = db.Column(db.DateTime)
+    followers = db.Column(db.Integer, nullable=False, default=0)
+
+    posts = db.relationship('Post', backref='page', lazy=True)
+
+    def __repr__(self):
+        return f"Page('{self.name})', '{self.description}' , '{self.followers}')"
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.VARCHAR(100), nullable=False)
+    content = db.Column(db.VARCHAR(100))
+    page = db.Column(db.Integer, db.ForeignKey(Page.id), nullable=False)
+    time_created = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"Post('{self.title})', '{self.content}')"
+
+
+
+
+
+
+
+
+
