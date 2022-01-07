e538fd8b609eba22c2fff0617448d884a255f871
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-07 12:52:31 +0000

    Added error classes in register.html and login.html for easier use in css. Made minor changes to the error messages.

--- auth/forms.py
+++ auth/forms.py
@@ -14,7 +14,7 @@ def check_password_special(form, field):
         if c in data:
             return
     raise ValidationError(
-        f"password must contain at least one special character ({special_characters})")  # if none found show error message
+        f"Password must contain at least one special character ({special_characters}).")  # if none found show error message
 
 
 def check_password_upper(form, field):
@@ -22,7 +22,7 @@ def check_password_upper(form, field):
     for c in uppercase_characters:  # iterate through and return if at least one upper case letter in data
         if c in data:
             return
-    raise ValidationError("password must contain at least one uppercase character")  # if none found show error message
+    raise ValidationError("Password must contain at least one uppercase character.")  # if none found show error message
 
 
 def check_password_lower(form, field):
@@ -30,7 +30,7 @@ def check_password_lower(form, field):
     for c in lowercase_characters:  # iterate through and return if at least one lower case letter in data
         if c in data:
             return
-    raise ValidationError("password must contain at least one lowercase character")  # if none found show error message
+    raise ValidationError("Password must contain at least one lowercase character.")  # if none found show error message
 
 
 def check_password_digit(form, field):
@@ -38,23 +38,23 @@ def check_password_digit(form, field):
     for c in digits:  # iterate through and return if at least one digit in data
         if c in data:
             return
-    raise ValidationError("password must contain at least one digit")  # if none found show error message
+    raise ValidationError("Password must contain at least one digit.")  # if none found show error message
 
 
 class RegisterForm(FlaskForm):
     # Set up fields
     email = StringField(validators=[DataRequired(), Email()])
     username = StringField(validators=[DataRequired(),
-                                        NoneOf(special_characters, "username must not contain any special "
-                                                                   "characters (%(values)s)")])
+                                        NoneOf(special_characters, "Username must not contain any special "
+                                                                   "characters (%(values)s).")])
 
     password = PasswordField(validators=[DataRequired(),
-                                         Length(6, 12, "password must be between 6 and 12 characters long"),
+                                         Length(6, 12, "Password must be between 6 and 12 characters long."),
                                          check_password_special,
                                          check_password_upper,
                                          check_password_lower,
                                          check_password_digit])
-    confirm_password = PasswordField(validators=[DataRequired(), EqualTo("password", "passwords do not match")])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo("password", "Passwords do not match.")])
     submit = SubmitField()
 
 
