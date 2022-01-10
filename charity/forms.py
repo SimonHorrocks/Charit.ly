3ab16ebf0d62e630773e72ee7ddcdbbeff2f4bbf
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-10 20:35:20 +0000

    Added button for adding posts. Updated PostForm.

--- charity/forms.py
+++ charity/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Required
+
+from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
-    title = StringField(validators=[Required()])
-    body = TextAreaField(validators=[Required()])
+    title = StringField(validators=[DataRequired()])
+    body = TextAreaField(validators=[DataRequired()])
     submit = SubmitField()
