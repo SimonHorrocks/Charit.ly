f31a3787098d58056c8924f1361e1510e3bf44ab
Simon <S.M.Horrocks2@newcastle.ac.uk>
2022-01-13 14:49:27 +0000

    fixed some database relationships

--- models.py
+++ models.py
@@ -6,17 +6,6 @@ from werkzeug.security import generate_password_hash
 from app import db
 
 
-def init_db():
-    db.drop_all()
-    db.create_all()
-    admin = User(username='admin',
-                 email='admin@email.com',
-                 password='Admin1!',
-                 roleID='admin')
-    db.session.add(admin)
-    db.session.commit()
-
-
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -36,13 +25,12 @@ class User(db.Model, UserMixin):
     pages = db.relationship('Page', backref='author', lazy=True)
     tags = db.relationship('Tag', secondary='interests', lazy='subquery', backref=db.backref('users', lazy='dynamic'))
 
-
     def __init__(self, username, email, password, roleID):
         self.username = username
         self.email = email
         self.password = generate_password_hash(password)
         self.roleID = roleID
-        self.registered_on = datetime.now()
+        self.registered_on = datetime.datetime.now()
         self.last_logged_in = None
         self.current_logged_in = None
 
@@ -54,14 +42,14 @@ class Page(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     time_created = db.Column(db.DateTime)
     followers = db.Column(db.Integer, nullable=False, default=0)
 
     posts = db.relationship('Post', backref='parent_page', lazy=True)
     # relationships
     tags = db.relationship('Tag', secondary='tags', lazy='subquery', backref=db.backref('pages', lazy=True))
-    events = db.relationship('Post', backref='event_page', lazy=True)
+    events = db.relationship('Event', backref='event_page', lazy=True)
 
     def __init__(self, name, description, user_id):
         self.name = name
@@ -77,7 +65,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.VARCHAR(100), nullable=False)
     content = db.Column(db.VARCHAR(100))
-    page = db.Column(db.Integer, db.ForeignKey('page.id'), nullable=False)
+    page = db.Column(db.Integer, db.ForeignKey(Page.id), nullable=False)
     time_created = db.Column(db.DateTime)
 
     def __repr__(self):
@@ -85,12 +73,12 @@ class Post(db.Model):
 
 
 class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(100))
     time = db.Column(db.DateTime)
     time_created = db.Column(db.DateTime)
-    Page = db.Column(db.Integer, db.ForeignKey(Page.id), nullable=False)
+    page = db.Column(db.Integer, db.ForeignKey(Page.id), nullable=False)
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
 
@@ -99,14 +87,14 @@ class Event(db.Model):
         self.description = description
         self.time = time
         self.time_created = datetime.datetime.now()
-        self.Page = page
+        self.page = page
         self.lat = lat
         self.lon = lon
 
 
 # Tags for pages to help with searching, user interests etc.
 class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     subject = db.Column(db.String(100), unique=True, nullable=False)
 
 
@@ -121,3 +109,14 @@ interests = db.Table('interests',
                      db.Column('tag_id', db.Integer, db.ForeignKey(Tag.id), primary_key=True),
                      db.Column('user_id', db.Integer, db.ForeignKey(User.id), primary_key=True)
                      )
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    admin = User(username='admin',
+                 email='admin@email.com',
+                 password='Admin1!',
+                 roleID='admin')
+    db.session.add(admin)
+    db.session.commit()
