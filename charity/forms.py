33dcff16d272696ba62233ba3be0a54a2e85f398
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-13 13:24:58 +0000

    Added new form for creating new events on charity pages.

--- charity/forms.py
+++ charity/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, DateField, IntegerField, DateTimeField
 
 from wtforms.validators import DataRequired
 
@@ -12,4 +12,13 @@ class PostForm(FlaskForm):
 
 class SearchForm(FlaskForm):
     search = StringField()
+    submit = SubmitField()
+
+
+class NewEventForm(FlaskForm):
+    name = StringField(validators=[DataRequired()])
+    description = TextAreaField(validators=[DataRequired()])
+    time = DateTimeField(format='%d%m%Y %H-%M-%s', validators=[DataRequired()])
+    lat = IntegerField(validators=[DataRequired()])
+    lon = IntegerField(validators=[DataRequired()])
     submit = SubmitField()
