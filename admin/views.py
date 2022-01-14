dcc9a9d13b320396d58fbe596a62054c72a2ca7a
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-14 10:58:41 +0000

    Updated admin view page - displayed the form for changing roles.

--- admin/views.py
+++ admin/views.py
@@ -1,14 +1,16 @@
 # IMPORTS
 import copy
-from flask_login import login_required, current_user
-from flask import Blueprint, render_template, request, flash
+from flask_login import login_required
+from flask import Blueprint, render_template
 from sqlalchemy import asc
 
+from admin.forms import RolesForm
 from app import db, requires_roles
 from models import User
 
 # CONFIG
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
+
 
 # VIEWS
 # view admin homepage
@@ -16,5 +18,9 @@ admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 @login_required
 @requires_roles('admin')
 def admin():
+    form = RolesForm()
+    if form.validate_on_submit():
+        User.query.filter_by(email=form.email.data).update({"roleID": form.role.data})
+        db.session.commit()
     users = User.query.order_by(asc('username')).all()
-    return render_template('admin.html', users=users)
+    return render_template('admin.html', users=users, form=form)
