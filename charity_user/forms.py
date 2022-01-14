09da4cf15f5770efb7ffab15cb36b323e5934bc9
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-14 17:31:44 +0000

    Added form for creating new charity pages.

---
+++ charity_user/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NewCharityPage(FlaskForm):
+    name = StringField(validators=[DataRequired()])
+    description = TextAreaField(validators=[DataRequired()])
+    submit = SubmitField()
