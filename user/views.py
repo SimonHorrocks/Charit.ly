03df8aade627e6e489467f0eda1853500f273395
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-17 13:11:01 +0000

    Added ability for normal users to change their username.

--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
 # IMPORTS
 
 from flask_login import login_required, current_user
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
 
 import app
 from app import db, requires_roles
-from models import Page
-
+from models import Page, User
+from user.forms import NameForm
 
 user_blueprint = Blueprint('user', __name__, template_folder='templates')
 
@@ -16,5 +16,15 @@ user_blueprint = Blueprint('user', __name__, template_folder='templates')
 @login_required
 @requires_roles('user')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', change_name_form=NameForm())
 
+
+@user_blueprint.route("/user_change_name", methods=["POST"])
+@requires_roles("user")
+def change_name():
+    form = NameForm()
+    if form.validate_on_submit:
+        User.query.filter_by(id=current_user.id).update({"username": form.name.data})
+        db.session.commit()
+
+    return redirect(url_for("user.profile"))
